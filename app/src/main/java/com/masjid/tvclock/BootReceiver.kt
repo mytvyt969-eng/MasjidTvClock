67e03a27ea04579127f8be113a077b5020bf1227
mytvyt969-eng <mytvyt969@gmail.com>
2026-09-22 07:47:48 +0530

    Add boot receiver

---
+++ app/src/main/java/com/masjid/tvclock/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.masjid.tvclock
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+class BootReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if(intent.action==Intent.ACTION_BOOT_COMPLETED) {
+            context.startActivity(Intent(context,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+        }
+    }
+}
